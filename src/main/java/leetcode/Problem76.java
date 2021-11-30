e84d52775bf2bb6a4c9f53505331c66f2c5c1e20
Tuan Nguyen <tuanna4284@gmail.com>
2021-11-30 21:28:09 +0700

    fix: problem 76

--- src/main/java/leetcode/Problem76.java
+++ src/main/java/leetcode/Problem76.java
@@ -1,49 +1,84 @@
 package leetcode;
 
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Set;
-import java.util.TreeSet;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Problem76 {
 
   public static String minWindow(String s, String t) {
-    Set<Character> chars = t.chars().mapToObj(e -> (char)e).collect(Collectors.toUnmodifiableSet());
-    TreeSet<CharIndex> tree = new TreeSet<>();
-    int count = 0;
-    int l =0, r =0, curMin = s.length();
-    int[] indexes = new int[128];
-    Arrays.fill(indexes, -1);
-    for (int i = 0; i < s.length(); i++) {
-      char curChar = s.charAt(i);
-      if (!chars.contains(curChar)) {
-        continue;
+    Map<Character, Integer> charCounts = new HashMap<>();
+    int patLen = t.length();
+    int minLen = s.length() + 1;
+    int minL = 0, minR = 0;
+    int l = 0, r = 0;
+    for (char c : t.toCharArray()) {
+      if (!charCounts.containsKey(c)) {
+        charCounts.put(c, 0);
       }
-
-      CharIndex newCharIndex = new CharIndex(curChar, i);
-      CharIndex forFinding = new CharIndex(curChar, indexes[curChar]);
-      indexes[curChar] = i;
-      if (!tree.contains(forFinding)) {
-        tree.add(newCharIndex);
-        count++;
-      } else {
-        tree.remove(forFinding);
-        tree.add(newCharIndex);
-      }
-
-      if (count == t.length()) {
-        int newMin = tree.last().index - tree.first().index;
-        if (newMin < curMin) {
-          curMin = newMin;
-          l = tree.first().index;
-          r = tree.last().index;
-        }
-      }
+      int curCount = charCounts.get(c);
+      charCounts.put(c, curCount + 1);
     }
 
-    if (count < t.length()) return "";
-    return s.substring(l, r + 1);
+    while (l < s.length()) {
+      for (; r < s.length(); r++) {
+        char curCh = s.charAt(r);
+        if (!charCounts.containsKey(curCh)) {
+          continue;
+        }
+        int chCount = charCounts.get(curCh);
+        charCounts.put(curCh, chCount - 1);
+        if (chCount > 0) {
+          patLen--;
+        }
+        if (patLen == 0) {
+          break;
+        }
+      }
+
+      if (patLen > 0) {// not found any
+        break;
+      }
+
+      if (r - l + 1 < minLen) {
+        minLen = r - l +1;
+        minL = l;
+        minR = r;
+      }
+
+      l++;
+      while (true) {
+        char curCh = s.charAt(l-1);
+        if (!charCounts.containsKey(curCh)) {
+          if ((r - l + 1) < minLen) {
+            minLen = (r - l + 1);
+            minL = l;
+            minR = r;
+          }
+          l++;
+          continue;
+        }
+        int chCount = charCounts.get(curCh);
+        charCounts.put(curCh, chCount+1);
+        if (chCount >= 0) {
+          patLen++;
+        }
+        if (patLen > 0) {
+          break;
+        }
+        if ((r - l + 1) < minLen) {
+          minLen = (r - l + 1);
+          minL = l;
+          minR = r;
+        }
+        l++;
+      }
+      r++;
+    }
+
+    if (minLen == s.length() + 1) {
+      return "";
+    }
+    return s.substring(minL, minR + 1);
   }
 
   public static void main(String[] args) {
@@ -53,37 +88,9 @@ public class Problem76 {
     String s4 = "AA";
     String t = "ABC";
     String t2 = "AA";
-//    System.out.println(minWindow(s, t));
+    System.out.println(minWindow(s, t));
 //    System.out.println(minWindow(s2, t));
 //    System.out.println(minWindow(s3, t));
     System.out.println(minWindow(s4, t2));
-  }
-
-  public static class CharIndex implements Comparable<CharIndex> {
-    char c;
-    int index;
-
-    public CharIndex(char c, int index) {
-      this.c = c;
-      this.index = index;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-      CharIndex charIndex = (CharIndex) o;
-      return c == charIndex.c;
-    }
-
-    @Override
-    public int hashCode() {
-      return Objects.hash(c, index);
-    }
-
-    @Override
-    public int compareTo(CharIndex o) {
-      return index - o.index;
-    }
   }
 }
