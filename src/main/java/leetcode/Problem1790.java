943aa190a61d8bb6df1bc1540f15b98bdddf139b
Tuan Nguyen <tuanna4284@gmail.com>
2023-07-01 20:10:32 +0700

    update problem

--- src/main/java/leetcode/Problem1790.java
+++ src/main/java/leetcode/Problem1790.java
@@ -15,71 +15,74 @@ public class Problem1790 {
         int l = 0;
         int r = cells.length;
         int last = l;
+        int matrixValue = 1; // 1 water, 0 land
         while (l < r) {
             int m = (r + l)/2; // day m
-            int[][] matrixAtDayM = buildUpMatrix(matrix, cells, l, m);
-            if (canReachDestination(matrixAtDayM)) {
+            if (matrixValue == 1) {
+                fixMatrix(matrix, cells, l, m, matrixValue);
+            } else {
+                fixMatrix(matrix, cells, m + 1, r, matrixValue);
+            }
+            if (canReachDestination(matrix)) {
                 last = m;
                 l = m + 1;
-                matrix = matrixAtDayM;
+                matrixValue = 1;
             } else {
                 r = m;
+                matrixValue = 0;
             }
         }
         return last;
     }
 
-    public int[][] buildUpMatrix(int[][] matrix, int[][] cells, int from, int to) {
-        int [][] copiedMatrix = clone(matrix);
-
+    // apply matrixValue on matrix
+    public int[][] fixMatrix(int[][] matrix, int[][] cells, int from, int to, int matrixValue) {
         for (int i = from; i <= to; i++) {
             int tmpR = cells[i][0] - 1;
             int tmpC = cells[i][1] - 1;
-            copiedMatrix[tmpR][tmpC] = 1;
+            matrix[tmpR][tmpC] = matrixValue;
         }
 
-        return copiedMatrix;
-    }
-
-    public int[][] clone(int[][] matrix) {
-        int [][] myInt = new int[matrix.length][];
-        for(int i = 0; i < matrix.length; i++)
-            myInt[i] = matrix[i].clone();
-        return myInt;
+        return matrix;
     }
 
     public boolean canReachDestination(int[][] matrix) {
-        for (int i = 0; i < matrix[0].length; i++)
-            if (canReachDestination(matrix, 0, i, 0))
+        boolean[][] visited = new boolean[matrix.length][matrix[0].length];
+        for (int ix = 0; ix < matrix.length; ix++)
+            Arrays.fill(visited[ix], false);
+        for (int i = 0; i < matrix[0].length; i++) {
+            if (canReachDestination2(matrix, 0, i, visited))
                 return true;
+        }
 
         return false;
     }
 
-    // direction: 0 -> down, 1 -> from right, 2 -> from left
-
-    public boolean canReachDestination(int[][] matrix, int r, int c, int direction) {
+    public boolean canReachDestination2(int[][] matrix, int r, int c, boolean[][] visited) {
         if (c < 0 || c >= matrix[0].length)
+            return false;
+        if (r < 0)
             return false;
         if (matrix[r][c] == 1)
             return false;
         if (r == matrix.length - 1)
             return true;
+        if (visited[r][c]) {
+            return false;
+        }
+        visited[r][c] = true;
 
-        if (canReachDestination(matrix, r + 1, c, 0))
-            return true;
-        if (direction == 0) {
-            return canReachDestination(matrix, r, c - 1, 1) ||  canReachDestination(matrix, r, c + 1, 2);
-        } else if (direction == 1) {
-            // if it is from right => only go to the left
-            return canReachDestination(matrix, r, c - 1, 1);
+        if (r == 0) {
+            return canReachDestination2(matrix, r + 1, c, visited);
         } else {
-            // if it is from left => only go to the right
-            return canReachDestination(matrix, r, c + 1, 2);
+            return canReachDestination2(matrix, r + 1, c, visited)
+                    || canReachDestination2(matrix, r, c - 1, visited)
+                    ||  canReachDestination2(matrix, r, c + 1, visited)
+                    || canReachDestination2(matrix, r- 1, c, visited);
         }
     }
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
         var sol = new Problem1790();
         int[][] cells = {{1,1},{2,1},{1,2},{2,2}};
         var result = sol.latestDayToCross(2, 2, cells);
